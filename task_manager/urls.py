208fb864bcdf894e6186ddfd4b0202543b5b5573
sriharsh05 <sriharsh.harsha5@gmail.com>
2024-01-02 16:22:08 +0530

    implement complete task feature

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 tasks = []
+completed_tasks = []
 
 def tasks_view(request):
     return render(request,"tasks.html",{"tasks" : tasks})
@@ -17,11 +19,22 @@ def delete_task_view(request, index):
     del tasks[index-1]
     return HttpResponseRedirect("/tasks")
 
+def completed_tasks_view(request):
+    return render(request, "completed_tasks.html", {"completed_tasks" : completed_tasks})
 
+def all_tasks_view(request):
+    return render(request, "all_tasks.html", {"completed_tasks" : completed_tasks, "tasks" : tasks})
+
+def complete_task_view(request, index):
+    completed_tasks.append(tasks.pop(index-1))
+    return HttpResponseRedirect("/tasks")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/',tasks_view),
-    path('tasks/add-task', add_task_view),
-    path('tasks/delete-task/<int:index>', delete_task_view)
+    path('tasks/', tasks_view),
+    path('add-task/', add_task_view),
+    path('delete-task/<int:index>/', delete_task_view),
+    path('complete_task/<int:index>/', complete_task_view),
+    path('completed_tasks/', completed_tasks_view),
+    path('all_tasks/', all_tasks_view),
 ]
