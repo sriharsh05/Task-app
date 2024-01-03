1b894cbe3ef20b3c9803bb5dff8761d6cb6e856c
sriharsh05 <sriharsh.harsha5@gmail.com>
2024-01-03 13:19:56 +0530

    change views location

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,33 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-
-tasks = []
-completed_tasks = []
-
-def tasks_view(request):
-    return render(request,"tasks.html",{"tasks" : tasks})
-
-def add_task_view(request):
-    tasks_value = request.GET.get("task")
-    tasks.append(tasks_value)
-    return HttpResponseRedirect("/tasks")
-
-def delete_task_view(request, index):
-    del tasks[index-1]
-    return HttpResponseRedirect("/tasks")
-
-def completed_tasks_view(request):
-    return render(request, "completed_tasks.html", {"completed_tasks" : completed_tasks})
-
-def all_tasks_view(request):
-    return render(request, "all_tasks.html", {"completed_tasks" : completed_tasks, "tasks" : tasks})
-
-def complete_task_view(request, index):
-    completed_tasks.append(tasks.pop(index-1))
-    return HttpResponseRedirect("/tasks")
+from tasks.views import add_task_view, all_tasks_view, complete_task_view, completed_tasks_view, delete_task_view, tasks_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
