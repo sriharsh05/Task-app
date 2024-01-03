25dfe9c48caed14c078d06857b2b8c01413c7d03
sriharsh05 <sriharsh.harsha5@gmail.com>
2024-01-03 13:10:58 +0530

    Added tasks table

--- tasks/models.py
+++ tasks/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now=True)
