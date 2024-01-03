fd2d27ebc7af963c50f7c92028bbd7ffa36bfd70
sriharsh05 <sriharsh.harsha5@gmail.com>
2024-01-03 16:11:13 +0530

    implement completed tasks using database tables

--- tasks/views.py
+++ tasks/views.py
@@ -3,12 +3,9 @@ from django.shortcuts import render
 
 from tasks.models import Task
 
-tasks = []
-completed_tasks = []
-
 def tasks_view(request):
     search_term = request.GET.get("search")
-    tasks = Task.objects.filter(deleted=False)
+    tasks = Task.objects.filter(completed=False).filter(deleted=False)
     if search_term:
         tasks = tasks.filter(title__icontains=search_term)
     return render(request,"tasks.html",{"tasks" : tasks})
@@ -22,11 +19,14 @@ def delete_task_view(request, index):
     return HttpResponseRedirect("/tasks")
 
 def completed_tasks_view(request):
+    completed_tasks = Task.objects.filter(completed=True).filter(deleted=False)
     return render(request, "completed_tasks.html", {"completed_tasks" : completed_tasks})
 
 def all_tasks_view(request):
+    tasks = Task.objects.filter(completed=False).filter(deleted=False)
+    completed_tasks = Task.objects.filter(completed=True).filter(deleted=False)
     return render(request, "all_tasks.html", {"completed_tasks" : completed_tasks, "tasks" : tasks})
 
 def complete_task_view(request, index):
-    completed_tasks.append(tasks.pop(index-1))
+    Task.objects.filter(id=index).update(completed=True)
     return HttpResponseRedirect("/tasks")
