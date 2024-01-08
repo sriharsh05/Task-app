25130b9c284b30bd8d306ff20674de8916b695bb
sriharsh05 <sriharsh.harsha5@gmail.com>
2024-01-08 15:47:22 +0530

    added priority column

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    priority = models.PositiveIntegerField(validators=[MinValueValidator(1)], blank=False, null=True)
     completed = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
