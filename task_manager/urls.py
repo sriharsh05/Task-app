d3523344f5db44322680b6d507fbdb7475301d85
sriharsh05 <sriharsh.harsha5@gmail.com>
2024-01-08 16:27:00 +0530

    revamp application

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -9,14 +9,14 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sessiontest',session_storage_view),
+    path('user/signup', UserCreateView.as_view()),
+    path('user/login', UserLoginView.as_view()),
+    path('user/logout', LogoutView.as_view()), 
     path('tasks/', GenericTaskView.as_view()),
     path('create-task/', GenericTaskCreateView.as_view()),
     path('update-task/<int:pk>/', GenericTaskUpdateView.as_view()),
     path('delete-task/<pk>/', TaskDeleteView.as_view()),
     path('complete_task/<pk>/', TaskCompleteView.as_view()),
-    path('completed_tasks/', CompletedTaskListView.as_view()),
-    path('sessiontest',session_storage_view),
-    path('user/signup', UserCreateView.as_view()),
-    path('user/login', UserLoginView.as_view()),
-    path('user/logout', LogoutView.as_view()), 
+    path('completed_tasks/', CompletedTaskListView.as_view()),    
 ]
